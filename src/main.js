b03b13ae28e9e49ca2388b419e17788543826314
Sora Arakawa <mail@arkw.net>
2023-05-05 18:34:30 +0900

    Install vue-gtag

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import VueGtag from 'vue-gtag'
 
 import './assets/main.css'
 
-createApp(App).mount('#app')
+createApp(App)
+  .use(VueGtag, { config: { id: import.meta.env.VITE_GTAG_ID } })
+  .mount('#app')
